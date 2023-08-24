b66cd1fbea9f2110bb6698adc99ffe7647bf0678
Michael Henke <michaelh1995@gmx.de>
2023-08-24 10:12:19 +0200

    change button name

--- components/shopping-cart/index.js
+++ components/shopping-cart/index.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function ShoppingCart({ toggle }) {
   return (
     <div>
-      <button onClick={toggle}>Click me!</button>
+      <button onClick={toggle}>View Order</button>
     </div>
   );
 }
