bc61a8cc4164246a66a0b7cf66ec7f2b27eb7708
Michael Henke <michaelh1995@gmx.de>
2023-08-25 14:21:54 +0200

    h2->h1

--- pages/check-out/index.js
+++ pages/check-out/index.js
@@ -1,7 +1,7 @@
 export default function Checkout({ selectedTable }) {
   return (
     <div>
-      <h2>Checkout Page</h2>
+      <h1>Checkout Page</h1>
       <p>Congratulations!</p>
       <p>Your Order will be delivered shortly to:</p>
       <p>Table Nr.: {selectedTable}</p>
