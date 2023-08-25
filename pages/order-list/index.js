37504691702ddded43cac3a5c5f8864f4972e662
Michael Henke <michaelh1995@gmx.de>
2023-08-25 10:37:00 +0200

    add backbutton

--- pages/order-list/index.js
+++ pages/order-list/index.js
@@ -3,6 +3,7 @@ import Drinks from "/components/order-menu/drink-menu";
 import Maindishes from "/components/order-menu/main-menu";
 import OrderOverview from "../order-overview";
 import Backdrop from "@/components/back-drop/index.js";
+import BackButton from "@/components/back-button";
 import { useState } from "react";
 
 export default function OrderList({ handleAmountChange, items }) {
@@ -26,6 +27,7 @@ export default function OrderList({ handleAmountChange, items }) {
       <OrderOverview items={items} show={drawerOpen} />
       {drawerOpen && <Backdrop close={backdropClickHandler} />}
       <ShoppingCart toggle={drawerToggleClickHandler} />
+      <BackButton />
     </>
   );
 }
